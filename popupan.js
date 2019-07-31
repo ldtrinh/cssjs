351bfa95e4a96359cf425ba944482b7c683584e7
ldtrinh <53522192+ldtrinh@users.noreply.github.com>
2019-07-31 21:12:08 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'https://www.facebook.con/nharebienhoa/';
+	var url_popup = 'https://www.facebook.com/nharebienhoa/';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
